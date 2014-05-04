259d90b02c5a1227c4d37144c18b3d3e64a2d91d
Axel Roebel <Axel.Roebel@ircam.fr>
2014-05-04 11:16:17 +0200

    added __all__

--- pysndfile/__init__.py
+++ pysndfile/__init__.py
@@ -2,3 +2,9 @@ from _pysndfile import PySndfile get_sndfile_version get_sndfile_formats get_snd
 
 import sndio
 import Faiff
+
+__all__ = [
+    "PySndfile", "get_sndfile_version", "get_sndfile_formats", "get_sndfile_encodings", "construct_format",
+    "encoding_id_to_name", "encoding_name_to_id", "fileformat_name_to_id", "fileformat_id_to_name", "endian_name_to_id",
+    "endian_id_to_name", "commands_name_to_id", "commands_id_to_name", "sndio", "Faiff"
+    ]
