3e2869021d63f3cf762f1f0191e460003d8eea54
roebel <axel.roebel@ircam.fr>
2019-12-18 12:14:08 +0100

    fixed realease number of documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Axel Roebel'
 author = 'Axel Roebel'
 
 # The short X.Y version
-version = '1.3'
+version = '1.4'
 # The full version, including alpha/beta/rc tags
-release = '1.3'
+release = '1.4'
 
 
 # -- General configuration ---------------------------------------------------
